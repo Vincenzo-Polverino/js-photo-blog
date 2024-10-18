6493b5db52b1e42ad44fc21c243058f079f11b3a
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-18 15:30:25 +0200

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,16 @@
 console.log('linked');
 const tilteEl = document.querySelectorAll('.title');
-const photoEl = document.querySelectorAll('.card'); // Modificato per selezionare l'elemento corretto
+const photoEl = document.querySelectorAll('.card');
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     .then(response => {
         const photos = response.data;
         photos.forEach((photo, i) => {
-            const { title, thumbnailUrl } = photo;
+            const { title, url } = photo;
 
             photoEl[i].innerHTML = `
                     <div class="card-body d-flex justify-content-center align-items-center">
-                        <img src="${thumbnailUrl}" alt="">
+                        <img src="${url}" alt="">
                     </div>
                     <p class="title">${title}</p>
                 `;
