e7fb10143f6add1a2deb179a014bd14363618a1e
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-18 12:27:07 +0200

    add partial script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,10 @@
 console.log('linked');
+
+
+const tilteEL = document.querySelectorAll('.title');
+
+axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
+    .then(response => {
+     
+    })
+    .catch(error => console.error('Errore:', error));
