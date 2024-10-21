f6c3fafbd38aeb630b7e2d3c1031ac1b5f5de8b3
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-21 11:46:54 +0200

    add close button

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,7 @@ const tilteEl = document.querySelectorAll('.title');
 const photoEl = document.querySelectorAll('.card');
 const modalEl = document.querySelectorAll('.modal')
 
+
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     .then(response => {
         const photos = response.data;
@@ -16,23 +17,34 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
                     <p class="title">${title}</p>
                 `;
 
-            photoEl.forEach(photo => {
-                photo.addEventListener("click", () => {
-                    document.body.innerHTML +=
-                        `
-            <div class="modal d-flex justify-content-center align-items-end">
-                <div>
-                    <div><img src="${url}" alt="foto"></div>
-                    <button type="button" class="btn btn-primary my-5">
-                        button
-                    </button>
-                </div>
-            </div>
+        });
+        
+        photoEl.forEach(photo => {
+            photo.addEventListener("click", () => {
     
-            `;
+                const url = photo.getAttribute('data-url');
+                document.body.innerHTML +=
+                    `
+                <div class="modal d-flex justify-content-center align-items-center">
+                    <div>
+                        <div class=" d-flex flex-column">
+                            <img src="${url}" alt="foto">
+                        <button type="button" class="close_btn btn btn-primary my-5">
+                            Chiudi
+                        </button>
+                        </div>
+                    </div>
+                </div>
+                `;
+                
+              const closeEl = document.querySelectorAll('.close_btn')
+                closeEl.forEach(button => {
+                    button.addEventListener("click", () => {
+                        document.querySelectorAll('.modal').forEach(modal => modal.remove());
+                        
+                    });
                 });
             });
         });
     })
     .catch(error => console.error('Errore:', error));
-
