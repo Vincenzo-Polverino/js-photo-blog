ef7ea4adf6addbd7a825e340c4b5e2bf26499fed
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-21 10:54:39 +0200

    fix modal event list position

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,7 @@
 console.log('linked');
 const tilteEl = document.querySelectorAll('.title');
 const photoEl = document.querySelectorAll('.card');
+const modalEl = document.querySelectorAll('.modal')
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     .then(response => {
@@ -15,14 +16,23 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
                     <p class="title">${title}</p>
                 `;
 
+            photoEl.forEach(photo => {
+                photo.addEventListener("click", () => {
+                    document.body.innerHTML +=
+                        `
+            <div class="modal d-flex justify-content-center align-items-end">
+                <div>
+                    <div><img src="${url}" alt="foto"></div>
+                    <button type="button" class="btn btn-primary my-5">
+                        button
+                    </button>
+                </div>
+            </div>
+    
+            `;
+                });
+            });
         });
     })
     .catch(error => console.error('Errore:', error));
 
-    let modal_display = document.querySelectorAll('.modal')
-
-    photoEl.forEach(photo => {
-        photo.addEventListener("click", () => {
-            document.body.innerHTML += ``;
-        });
-    });
